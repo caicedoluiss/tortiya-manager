c6a0a8d6da882be0cd2835b6a95532cc7f6391ff
caicedopluiss <lusecapi@gmail.com>
2025-06-11 18:52:28 -0500

    #48 - Add Notifications provider from toolpad

--- client/WebApp/src/hooks/useOrders.tsx
+++ client/WebApp/src/hooks/useOrders.tsx
@@ -1,38 +1,46 @@
 import { useCallback } from "react";
 import { getOrdersByDate, postOrder } from "../api/v1/orders";
 import type { ErrorResponse } from "../types/errorResponse";
-import { BAD_REQUEST_RESPONSE_STATUS } from "../api/errorResponseCodes";
 import type { Order } from "../types/Order";
+import { useNotifications } from "@toolpad/core/useNotifications";
 
 export default function useOrders() {
-    const getAllByDate = useCallback(async (date: Date) => {
-        try {
-            const { orders } = await getOrdersByDate(date);
-            return orders;
-        } catch (error) {
-            const axiosError = (error as ErrorResponse) || {};
-            const { code, validationErrors = [], message } = axiosError;
-            alert(`${code}: ${message}`);
-            if (code === BAD_REQUEST_RESPONSE_STATUS && validationErrors?.length > 0) {
-                console.error(JSON.stringify(validationErrors));
+    const notifications = useNotifications();
+    const getAllByDate = useCallback(
+        async (date: Date) => {
+            try {
+                const { orders } = await getOrdersByDate(date);
+                return orders;
+            } catch (error) {
+                const axiosError = (error as ErrorResponse) || {};
+                const { code, message } = axiosError;
+                notifications.show(`Application request error: '${code}'.${!message ? "" : ` ${message}`}`, {
+                    autoHideDuration: 5000,
+                    severity: "error",
+                });
+                return [];
             }
-            return [];
-        }
-    }, []);
+        },
+        [notifications],
+    );
 
-    const create = useCallback(async (order: Order) => {
-        try {
-            const resultOrder = await postOrder(order);
-            return resultOrder;
-        } catch (error) {
-            const axiosError = (error as ErrorResponse) || {};
-            const { code, validationErrors = [], message } = axiosError;
-            alert(`${code}: ${message}`);
-            if (code === BAD_REQUEST_RESPONSE_STATUS && validationErrors?.length > 0) {
-                console.error(JSON.stringify(validationErrors));
+    const create = useCallback(
+        async (order: Order) => {
+            try {
+                const resultOrder = await postOrder(order);
+                notifications.show("Orden registrada exitosamente.", { autoHideDuration: 3000, severity: "success" });
+                return resultOrder;
+            } catch (error) {
+                const axiosError = (error as ErrorResponse) || {};
+                const { code, message } = axiosError;
+                notifications.show(`Application request error: '${code}'.${!message ? "" : ` ${message}`}`, {
+                    autoHideDuration: 5000,
+                    severity: "error",
+                });
             }
-        }
-    }, []);
+        },
+        [notifications],
+    );
 
     return { getAllByDate, create };
 }
